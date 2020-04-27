834dcb08bed4f9781810372ac4fb4d11ea383e4d
joeli <joelimgu@gmail.com>
2020-04-27 10:32:40 +0200

    GUI Working with kotlin variables created

--- APP/app/src/main/java/com/example/j/MainActivity.kt
+++ APP/app/src/main/java/com/example/j/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.j
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textToSendTextBox = findViewById<EditText>(R.id.textToSendTextBox)
+        val outputText = findViewById<TextView>(R.id.outputText)
+        val sendButton = findViewById<Button>(R.id.sendButton)
+        sendButton.setOnClickListener{
+            outputText.text = textToSendTextBox.text
+        }
     }
 }
