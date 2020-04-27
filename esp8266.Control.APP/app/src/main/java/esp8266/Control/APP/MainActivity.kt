5bbe17d55c7d43da6a76da913231240f9ef3af01
joeli <joelimgu@gmail.com>
2020-04-27 20:07:37 +0200

    Firebase implemented ans working
    I am able to read and write strings (other types also I suppose not tested) ready to implement LED control

--- esp8266.Control.APP/app/src/main/java/esp8266/Control/APP/MainActivity.kt
+++ esp8266.Control.APP/app/src/main/java/esp8266/Control/APP/MainActivity.kt
@@ -1,10 +1,20 @@
 package esp8266.Control.APP
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +29,32 @@ class MainActivity : AppCompatActivity() {
         sendButton.setOnClickListener{
             outputText.text = textToSendTextBox.text
         }
+
+
+
+
+        // Write a message to the database
+
+        // Write a message to the database
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("2")
+
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.value
+                outputText.text = value as String
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+            }
+        })
+
+
     }
 }
